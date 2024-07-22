23f6105630f70340692d2f9f3a51ab1a099d3d0b
Daria <dashalokomoko30@gmail.com>
2024-07-22 00:37:20 -0400

    Add kilometer to mile conversion functionality

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,9 +6,17 @@ public class Main {
         double miles = 10.0;
         double kilometers = milesToKilometers(miles);
         System.out.println(miles + " miles is " + kilometers + " kilometers");
+
+        double kilometers2 = 16.0934;
+        double miles2 = kilometersToMiles(kilometers2);
+        System.out.println(kilometers2 + " kilometers is " + miles2 + " miles");
     }
 
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
+    }
+
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
     }
 }
