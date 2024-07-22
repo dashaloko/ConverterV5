c23f1c67b9edde6a5d79e0ca5e7bb5ca54299971
Daria <dashalokomoko30@gmail.com>
2024-07-22 00:34:17 -0400

    Kilometers onverter functionality

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,14 @@
 package app;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("My app.");
+        System.out.println("Converter Project: Mile to Kilometer and Kilometer to Mile Conversion");
+        double miles = 10.0;
+        double kilometers = milesToKilometers(miles);
+        System.out.println(miles + " miles is " + kilometers + " kilometers");
+    }
+
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
     }
 }
